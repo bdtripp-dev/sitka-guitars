e71ff5a7e5c9a7259f521236cfd74204f18a865b
Brian Tripp <btripp365@gmail.com>
2026-03-12 16:20:47 -0500

    feat: begin to containerize local version

---
+++ wp/wp-content/plugins/nextgen-gallery/static/IGW/Block/build/post-thumbnail.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('regenerator-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'e6cad119938aff388c69');
---
+++ wp/wp-content/plugins/social-icons-widget-by-wpzoom/block/dist/wpzoom-social-icons.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-token-list', 'wp-url'), 'version' => '381e6e5b7ca8fac25dfc1c4e4bf62a67');
